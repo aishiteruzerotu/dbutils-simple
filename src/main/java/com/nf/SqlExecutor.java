a5bbbbd1dbe081c5e4befa0de0b760a4a5dfeb62
杨天晨 <3138864192@qq.com>
2023-03-31 10:05:21 +0800

    声明query方法

--- src/main/java/com/nf/SqlExecutor.java
+++ src/main/java/com/nf/SqlExecutor.java
@@ -20,7 +20,7 @@ public class SqlExecutor extends AbstractSqlExecutor{
      * 创建对象
      * @param ds DataSource对象
      */
-    public SqlExecutor(DataSource ds) {
+    public SqlExecutor(final DataSource ds) {
         super(ds);
     }
 
@@ -86,7 +86,7 @@ public class SqlExecutor extends AbstractSqlExecutor{
             rows = statement.executeUpdate();
         }catch (SQLException e){
             //抛出更新失败异常
-            throw new DaoException("update be defeated...",e);
+            throw new DaoException("update defeated...",e);
         }finally {
             //关闭执行器 statement
             ResourceCleanerUtils.closeQuietly(statement);
@@ -98,6 +98,78 @@ public class SqlExecutor extends AbstractSqlExecutor{
         }
         //返回被修改的行数 rows
         return rows;
+    }
+
+    /**
+     * 对数据库进行更新操作，并返回一个被修改的行数
+     * @param conn 数据库连接
+     * @param closeConn 是否关闭数据库连接，输入 true 值关闭数据库连接，false 则不关闭
+     * @param sql sql更新语句
+     * @param params 访问参数
+     * @return 被影响的行数
+     */
+    private <T> T query(final Connection conn,final boolean closeConn,final String sql,final ResultSetHandler<T> rsh,final Object... params){
+        //判断数据库连接是否为空
+        if (conn==null){
+            //为空抛出异常
+            throw new DaoException("Connection is null...");
+        }
+        //判断sql语句是否为空
+        if (sql==null||sql.trim().length()==0){
+            //判断是否需要关闭连接
+            if (closeConn){
+                //为真关闭连接
+                ResourceCleanerUtils.closeQuietly(conn);
+            }
+            //为空抛出异常
+            throw new DaoException("Connection is null...");
+        }
+        //判断ResultSetHandler对象是否为空
+        if (rsh==null){
+            //判断是否需要关闭连接
+            if (closeConn){
+                //为真关闭连接
+                ResourceCleanerUtils.closeQuietly(conn);
+            }
+            //为空抛出异常
+            throw new DaoException("ResultSetHandler is null...");
+        }
+        //声明PreparedStatement对象 statement 为空
+        PreparedStatement statement = null;
+        //声明返回结果集为空
+        ResultSet rs = null;
+        //声明返回的对象为空
+        T result = null;
+        try {
+            //从数据库连接中获取 PreparedStatement 对象 并赋值给 statement
+            statement = conn.prepareStatement(sql);
+            //给执行器 statement 填入参数
+            statement = this.fillStatement(statement,params);
+            //使用statement执行器的.executeQuery() 查询方法，得到查询结果集
+            rs = statement.executeQuery();
+            //使用 ResultSetHandler 对象的 handler 方法，得到一个返回对象
+            result = rsh.handler(rs);
+        }catch (SQLException e){
+            //抛出更新失败异常
+            throw new DaoException("query defeated...",e);
+        }finally {
+            try {
+                //关闭结果集
+                ResourceCleanerUtils.close(rs);
+                //关闭执行器 statement
+                ResourceCleanerUtils.closeQuietly(statement);
+                //判断是否需要关闭连接
+                if (closeConn){
+                    //为真关闭连接
+                    ResourceCleanerUtils.closeQuietly(conn);
+                }
+            }catch (SQLException e){
+                //抛出资源关闭异常
+                throw new DaoException("Resource shutdown failed...",e);
+            }
+        }
+        //返回对象 result
+        return result;
     }
 
     /**
@@ -136,7 +208,7 @@ public class SqlExecutor extends AbstractSqlExecutor{
      * @param <T> 可以接收泛型对象
      * @return 一个被修改的行数
      */
-    public <T> int insertObject(Connection conn,T t){
+    public <T> int insertObject(final Connection conn,T t){
         //获取插入语句
         String sql = DbUtils.generateInsert(t);
         //调用自身 insertObject 方法
@@ -151,7 +223,7 @@ public class SqlExecutor extends AbstractSqlExecutor{
      * @param <T> 可以接收泛型对象
      * @return 一个被修改的行数
      */
-    public <T> int insertObject(Connection conn,final String sql,T t){
+    public <T> int insertObject(final Connection conn,final String sql,T t){
         //调用自身 insertObject 方法
         return this.insertObject(conn,true,sql,t);
     }
@@ -171,6 +243,5 @@ public class SqlExecutor extends AbstractSqlExecutor{
         //调用自身的 .update() 方法 , 进行更新操作
         return this.update(conn,closeConn,sql,objects);
     }
-
 
 }
