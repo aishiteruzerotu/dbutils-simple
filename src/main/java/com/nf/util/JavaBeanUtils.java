6bf864642af1a282903b99c8f71d3bc132b77eec
杨天晨 <3138864192@qq.com>
2023-04-12 16:10:28 +0800

    字段与属性之间的关系处理

--- src/main/java/com/nf/util/JavaBeanUtils.java
+++ src/main/java/com/nf/util/JavaBeanUtils.java
@@ -25,6 +25,17 @@ public class JavaBeanUtils {
         try {
             //通过 Introspector 的 getBeanInfo 方法回去 BeanInfo 对象
             //该方法用于解析 bean 对象，该对象需要包含其字段的 getter 或 setter方法
+            //实际上有 getter 或 setter 方法 ，就会生成一个对应的 属性
+            /**
+             * 如下详情
+             *  public Integer getXxx() {
+             *      return 1;
+             *  }
+             *  像这样的方法，就会自动的生成一个对应的 PropertyDescriptor 属性
+             *  这个属性 获取其名称的时候就会是 Xxx 的一个属性名称
+             *  实际上我们的实体类本身没有这样一个字段，如果调用 getDeclaredField 这个方法，就会抛出异常
+             *  所以我们要把这个异常给吞掉
+             */
             //否则不会有该字段的 PropertyDescriptor 属性信息
             beanInfo = Introspector.getBeanInfo(clz);
         } catch (IntrospectionException e) {
@@ -46,8 +57,22 @@ public class JavaBeanUtils {
             //获取字段信息
             return clz.getDeclaredField(name);
         } catch (NoSuchFieldException e) {
-            //抛出无法获取字段信息异常
-            throw new ReflexException("Unable to obtain field...",e);
+            //不要抛出异常，因为这个方法是通过属性的名称来获取字段
+            //实际上有 getter 或 setter 方法 ，就会生成一个对应的 属性
+            /**
+             * 如下详情
+             *  public Integer getXxx() {
+             *      return 1;
+             *  }
+             *  像这样的方法，就会自动的生成一个对应的 PropertyDescriptor 属性
+             *  这个属性 获取其名称的时候就会是 Xxx 的一个属性名称
+             *  实际上我们的实体类本身没有这样一个字段，如果调用 getDeclaredField 这个方法，就会抛出异常
+             *  所以我们要把这个异常给吞掉
+             */
+//            //抛出无法获取字段信息异常
+//            throw new ReflexException("Unable to obtain field...",e);
         }
+        //查不到对应的字段，则抛出空
+        return null;
     }
 }
